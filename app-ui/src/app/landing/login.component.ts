dedddbbcb913e3ebe8fe665892d5f91e924a7ea9
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-11-20 18:06:55 +0700

    Perfect login page

--- app-ui/src/app/landing/login.component.ts
+++ app-ui/src/app/landing/login.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 import {faBars, faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
 import {User} from "./signup/signup.component";
 import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -12,17 +13,30 @@ import {HttpClient} from "@angular/common/http";
 export class LoginComponent implements OnInit {
 
   list = ['Sign Up', 'Exit'];
+  url = ['/landing/signup', '#'];
   faBars = faBars;
   faArrowLeft = faArrowLeft;
   faGithub = faGithub;
   user = new User('', '', '');
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
   signin() {
+    const params = {
+      email: this.user.email,
+      password: this.user.password
+    }
+    this.http.post('/login', params).subscribe(r => {
+      console.log(r);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
+  navTo(i: number) {
+    this.router.navigate([this.url[i]]);
   }
 
 }
