e2022666fe604db4c6f998354db87130d8ea6e1f
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-11-20 23:08:24 +0700

    Add HttpClientModule
    - Help components within the module recognize and able to use HttpClient

--- app-ui/src/app/landing/landing.module.ts
+++ app-ui/src/app/landing/landing.module.ts
@@ -9,6 +9,7 @@ import {MatListModule} from "@angular/material/list";
 import {CommonModule} from "@angular/common";
 import {LoginComponent} from "./login.component";
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {FormsModule} from "@angular/forms";
     MatSidenavModule,
     MatListModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: []
 })
