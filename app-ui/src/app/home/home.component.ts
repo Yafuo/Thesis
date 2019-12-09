ad1bc6ad1bfd7dfb228b34ca6536452b87be5dbb
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-12-09 08:22:09 +0700

    Implement online payment & new structure of parking slots table

--- app-ui/src/app/home/home.component.ts
+++ app-ui/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit, Renderer2, ViewEncapsulation} from '@angular/core';
-import {faChevronDown, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faChevronDown, faSearch, faCheckCircle} from "@fortawesome/free-solid-svg-icons";
 import {TranslateService} from "@ngx-translate/core";
 import {FormControl} from "@angular/forms";
 import {Observable} from "rxjs";
 import {map, startWith} from "rxjs/operators";
+import * as CryptoJS from "crypto-js";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -15,20 +17,25 @@ export class HomeComponent implements OnInit {
 
   faChevronDown = faChevronDown;
   faSearch = faSearch;
+  faCheckCircle = faCheckCircle;
   districtList = ['Tan Phu', 'Tan Binh', 'Phu Nhuan', 'Binh Thanh'];
   googleMapsList = ['10 Ho Dac Di, P.Tay Thanh, Q.Tan Phu', '22/44 CMT8, P.2, Q.Tan Binh',
     '49a Phan Dang Luu, P.7, Q.Phu Nhuan', '96 Le Quang Dinh, P.14, Q.Binh Thanh'];
   selectedDistrict = 'Tan Phu';
   myControl = new FormControl();
-  filterList : Observable<string[]>;
+  filterList: Observable<string[]>;
   filterMapsList: Observable<string[]>;
   isFilterClicked = false;
-  packageList = ['1 hour', '3 hour', '1 day'];
+  packageList = [{name: '1 hour', cost: '5000'}, {name: '3 hour', cost: '20000'}, {name: '1 day', cost: '50000'}];
+  selectedPackage = {name: '', cost: ''};
   height = '';
   state = 'down';
   arriveTime = new Date(Date.now());
   leaveHomeTime = new Date(Date.now());
-  constructor(private translate: TranslateService, private render: Renderer2) { }
+  isShowResult = false;
+
+  constructor(private translate: TranslateService, private render: Renderer2, private http: HttpClient) {
+  }
 
   ngOnInit() {
     this.districtList = this.districtList.map(d => this._getTranslation(d));
@@ -42,12 +49,47 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  filter() {
+    this.toggleFilter();
+    this.isShowResult = true;
+  }
+
+  private _book() {
+    let date = Date.now().toString(10);
+    const d = {
+      partnerCode: 'MOMO',
+      accessKey: 'F8BBA842ECF85',
+      requestId: 'UIT' + date,
+      amount: this.selectedPackage.cost,
+      orderId: 'UIT' + date,
+      orderInfo: this.selectedPackage.name,
+      returnUrl: 'https://820d7b05.ngrok.io',
+      notifyUrl: 'https://820d7b05.ngrok.io/receive-notify',
+      requestType: 'captureMoMoWallet',
+      extraData: 'from uit.smartparking@gmail.com',
+      signature: ''
+    }
+    var data = `partnerCode=${d.partnerCode}&accessKey=${d.accessKey}&requestId=${d.requestId}&amount=${d.amount}&orderId=${d.orderId}&orderInfo=${d.orderInfo}&returnUrl=${d.returnUrl}&notifyUrl=${d.notifyUrl}&extraData=${d.extraData}`;
+    var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
+    var signature = CryptoJS.HmacSHA256(data, secretKey);
+    d.signature = signature.toString();
+    console.log(d.signature + '\n' + signature);
+    this.http.post<any>('https://test-payment.momo.vn/gw_payment/transactionProcessor', JSON.stringify(d)).subscribe(r => {
+      const prefix = 'https://test-payment.momo.vn/gw_payment/qrcode/image/receipt?key=';
+      const qrUrl = prefix + r.qrCodeUrl.slice(42);
+    });
+  }
+
+  onSelect(p) {
+    this.selectedPackage = p;
+  }
+
   private _filter(value: string, opt: number): string[] {
     const filterValue = value.toLowerCase();
     return opt === 1 ? this.districtList.filter(d => d.toLowerCase().indexOf(filterValue) > -1) : this.googleMapsList.filter(d => d.toLowerCase().indexOf(filterValue) > -1);
   }
 
-  private _getTranslation(value: string) : string {
+  private _getTranslation(value: string): string {
     let wordTranslated = 'not_ready';
     this.translate.get(`DISTRICT.${value}`).subscribe(word => {
       wordTranslated = word;
@@ -69,7 +111,7 @@ export class HomeComponent implements OnInit {
 
   display(stt: string) {
     // this.render.setStyle(document.body.getElementsByClassName('indicator'), 'transform', 'rotate(180deg)');
-    this.state = this.state === 'down' ? 'up' :'down';
+    this.state = this.state === 'down' ? 'up' : 'down';
   }
 
 }
--- models/parkingSlot.js
+++ models/parkingSlot.js
@@ -1,13 +1,22 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-const parkingSlotSchema = mongoose.Schema({
-    userId: String,
-    orderNum: Number,
-    status: {
-        isBooked: Boolean,
-        bookedTime: Date
-    }
-});
-
-
-module.exports = mongoose.model('parkingslot', parkingSlotSchema);
+const parkingSlotSchema = mongoose.Schema([{
+    _id: Object,
+    parkingAddress: String,
+    current: {
+        userName: String,
+        status: String,
+        startTime: Date,
+        package: Number,
+        endTime: Date
+    },
+    future: [
+        {
+            userName: String,
+            status: String,
+            startTime: Date,
+            package: Number,
+            endTime: Date
+        }
+    ]
+}]);
