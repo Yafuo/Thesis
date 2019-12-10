b667de1a9bed35c7d63b27987a0328894525f4df
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-12-10 18:28:22 +0700

    Implement Filter feature
    - Prepare almost everything to get ready for Filter service.

--- app-ui/src/app/home/home.component.ts
+++ app-ui/src/app/home/home.component.ts
@@ -20,12 +20,18 @@ export class HomeComponent implements OnInit {
   faSearch = faSearch;
   faCheckCircle = faCheckCircle;
   districtList = ['Tan Phu', 'Tan Binh', 'Phu Nhuan', 'Binh Thanh'];
-  googleMapsList = ['10 Ho Dac Di, P.Tay Thanh, Q.Tan Phu', '22/44 CMT8, P.2, Q.Tan Binh',
+  parkingStationList = ['10 Ho Dac Di, P.Tay Thanh, Q.Tan Phu', '22/44 CMT8, P.2, Q.Tan Binh',
     '49a Phan Dang Luu, P.7, Q.Phu Nhuan', '96 Le Quang Dinh, P.14, Q.Binh Thanh'];
-  selectedDistrict = 'Tan Phu';
-  myControl = new FormControl();
-  filterList: Observable<string[]>;
-  filterMapsList: Observable<string[]>;
+  userLocation = ['60/22 Dong Den, P.14, Q.Tan Binh', '199 Truong Dinh, P.5, Q.3',
+    '70 Hoang Van Thu, P.10, Q.Phu Nhuan', '67 Ly Thuong Kiet, P.3, Q.Tan Binh'];
+  selectedParkingStation = 'not_choose';
+  selectedUserLocation = 'not_choose';
+  userLocationControl = new FormControl();
+  parkingStationControl = new FormControl();
+  allSearch = new FormControl();
+  filterAllSearchList: Observable<string[]>;
+  filterUserLocationList: Observable<string[]>;
+  filterparkingStationList: Observable<string[]>;
   isFilterClicked = false;
   packageList = [{name: '1 hour', cost: '5000'}, {name: '3 hour', cost: '20000'}, {name: '1 day', cost: '50000'}];
   selectedPackage = {name: '', cost: ''};
@@ -34,8 +40,9 @@ export class HomeComponent implements OnInit {
   arriveTime = new Date(Date.now());
   leaveHomeTime = new Date(Date.now());
   isShowResult = false;
+  isCurrentLocationChecked = false;
   qrUrl= '';
-  isStake = true;
+  isStake = false;
   socket: SocketIOClient.Socket;
 
   constructor(private translate: TranslateService, private render: Renderer2, private http: HttpClient) {
@@ -44,13 +51,17 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.districtList = this.districtList.map(d => this._getTranslation(d));
-    this.filterList = this.myControl.valueChanges.pipe(
+    this.filterAllSearchList = this.allSearch.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value, 1))
     );
-    this.filterMapsList = this.myControl.valueChanges.pipe(
+    this.filterUserLocationList = this.userLocationControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value, 2))
+    );
+    this.filterparkingStationList = this.parkingStationControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value, 3))
     );
   }
 
@@ -58,12 +69,17 @@ export class HomeComponent implements OnInit {
     this.socket = io.connect('http://localhost:3000');
     this.socket.on('news', (news: any) => {
       this.qrUrl = '';
+      this.isStake = true;
     });
   }
 
   filter() {
     this.toggleFilter();
     this.isShowResult = true;
+    this.selectedParkingStation = this.parkingStationControl.value;
+    console.log(this.selectedParkingStation);
+    this.selectedUserLocation = this.isCurrentLocationChecked ? '14 Tran Van On, P.Tay Thanh, Q.Tan Phu' : this.userLocationControl.value;
+    console.log(this.selectedUserLocation);
   }
 
   private _book() {
@@ -98,7 +114,9 @@ export class HomeComponent implements OnInit {
 
   private _filter(value: string, opt: number): string[] {
     const filterValue = value.toLowerCase();
-    return opt === 1 ? this.districtList.filter(d => d.toLowerCase().indexOf(filterValue) > -1) : this.googleMapsList.filter(d => d.toLowerCase().indexOf(filterValue) > -1);
+    if (opt === 1) { return this.districtList.filter(d => d.toLowerCase().indexOf(filterValue) > -1); }
+    else if (opt === 2) { return this.userLocation.filter(d => d.toLowerCase().indexOf(filterValue) > -1); }
+    else { return this.parkingStationList.filter(d => d.toLowerCase().indexOf(filterValue) > -1);}
   }
 
   private _getTranslation(value: string): string {
